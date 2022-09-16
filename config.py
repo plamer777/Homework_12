d056eb1a2aa36a693dcf2eb30a9e123e4e660532
Plamer <plamer88@yandex.ru>
2022-09-16 21:50:02 +0300

    add doc string in config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+"""This unit contains all named constants for Flask app"""
 from os import path
-
+# --------------------------------------------------------------------------
 ALLOWED = {'jpg', 'jpeg', 'gif', 'png'}
 POST_SOURCE = path.join('data', 'posts.json')
 LOG_PATH = path.join('logs', 'app_logs.log')
